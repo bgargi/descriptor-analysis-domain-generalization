d573659fbb104dd36d9bdc10728c441e12c89b7f
Gargi Balasubramaniam <gargib2@illinois.edu>
2022-05-11 13:35:20 +0530

    feat(groupdro_dann): adding support for GroupDRO, DANN

--- descriptor_extraction.py
+++ descriptor_extraction.py
@@ -1,11 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-
-# python3 descriptor_extraction.py --data_dir=./domainbed/data/ --output_dir=./domainbed/ermnew/ --algorithm ERM --dataset DomainNet
-# python3 descriptor_extraction.py --data_dir=./domainbed/data/ --output_dir=./domainbed/ermnew/ --algorithm Mixup --dataset DomainNet
-
-
-
+# Authored by Gargi Balasubramaniam
 
 import argparse
 import collections
@@ -15,8 +9,10 @@ import random
 import sys
 import time
 import uuid
+import pickle
 
 import numpy as np
+import PIL
 import torch
 import torchvision
 import torch.utils.data
@@ -26,11 +22,15 @@ from domainbed import hparams_registry
 from domainbed import algorithms
 from domainbed.lib import misc
 from domainbed.lib.fast_data_loader import InfiniteDataLoader, FastDataLoader
-from torchsummary import summary
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Domain generalization')
     parser.add_argument('--data_dir', type=str)
+
+    ## FOR ISR Parsed Data
+    parser.add_argument('--load_model_dir', type=str)
+    parser.add_argument('--save_parsed_data_dir', type=str)
+
     parser.add_argument('--dataset', type=str, default="RotatedMNIST")
     parser.add_argument('--algorithm', type=str, default="ERM")
     parser.add_argument('--task', type=str, default="domain_generalization",
@@ -73,6 +73,7 @@ if __name__ == "__main__":
     print("\tCUDA: {}".format(torch.version.cuda))
     print("\tCUDNN: {}".format(torch.backends.cudnn.version()))
     print("\tNumPy: {}".format(np.__version__))
+    print("\tPIL: {}".format(PIL.__version__))
 
     print('Args:')
     for k, v in sorted(vars(args).items()):
@@ -119,88 +120,155 @@ if __name__ == "__main__":
     # domain generalization and domain adaptation results, then domain
     # generalization algorithms should create the same 'uda-splits', which will
     # be discared at training.
+
     in_splits = []
     out_splits = []
-    uda_splits = []
+
+    # print(dataset)
     for env_i, env in enumerate(dataset):
-        uda = []
+        # print(env_i)#, env)
 
         out, in_ = misc.split_dataset(env,
             int(len(env)*args.holdout_fraction),
             misc.seed_hash(args.trial_seed, env_i))
 
-        if env_i in args.test_envs:
-            uda, in_ = misc.split_dataset(in_,
-                int(len(in_)*args.uda_holdout_fraction),
-                misc.seed_hash(args.trial_seed, env_i))
+        in_weights, out_weights, uda_weights = None, None, None
 
-        if hparams['class_balanced']:
-            in_weights = misc.make_weights_for_balanced_classes(in_)
-            out_weights = misc.make_weights_for_balanced_classes(out)
-            if uda is not None:
-                uda_weights = misc.make_weights_for_balanced_classes(uda)
-        else:
-            in_weights, out_weights, uda_weights = None, None, None
         in_splits.append((in_, in_weights))
         out_splits.append((out, out_weights))
-        if len(uda):
-            uda_splits.append((uda, uda_weights))
-
-    if args.task == "domain_adaptation" and len(uda_splits) == 0:
-        raise ValueError("Not enough unlabeled samples for domain adaptation.")
 
     train_loaders = [InfiniteDataLoader(
         dataset=env,
         weights=env_weights,
         batch_size=hparams['batch_size'],
         num_workers=dataset.N_WORKERS)
-        for i, (env, env_weights) in enumerate(in_splits)
-        if i not in args.test_envs]
+        for i, (env, env_weights) in enumerate(in_splits)]
 
-    uda_loaders = [InfiniteDataLoader(
-        dataset=env,
-        weights=env_weights,
-        batch_size=hparams['batch_size'],
-        num_workers=dataset.N_WORKERS)
-        for i, (env, env_weights) in enumerate(uda_splits)
-        if i in args.test_envs]
 
     eval_loaders = [FastDataLoader(
         dataset=env,
         batch_size=64,
         num_workers=dataset.N_WORKERS)
-        for env, _ in (in_splits + out_splits + uda_splits)]
-    eval_weights = [None for _, weights in (in_splits + out_splits + uda_splits)]
+        for env, _ in (in_splits + out_splits)]
+    eval_weights = [None for _, weights in (in_splits + out_splits )]
     eval_loader_names = ['env{}_in'.format(i)
         for i in range(len(in_splits))]
     eval_loader_names += ['env{}_out'.format(i)
         for i in range(len(out_splits))]
-    eval_loader_names += ['env{}_uda'.format(i)
-        for i in range(len(uda_splits))]
+
 
     algorithm_class = algorithms.get_algorithm_class(args.algorithm)
     algorithm = algorithm_class(dataset.input_shape, dataset.num_classes,
         len(dataset) - len(args.test_envs), hparams)
 
-    #### - Make sure to change for algo specific parameters below as shown in comments
+    algorithm.to(device)
 
-    # Change to "erm/model.." for ERM model 
-    PATH = "./domainbed/erm/model.pkl"
-    model_sample = algorithm
-    model_sample = model_sample.to(device)
-    model_sample.load_state_dict(torch.load(PATH)['model_dict'])
-    print(model_sample.featurizer)
-    feat_extr = model_sample.featurizer.to(device)    
+    # IF group DRO, add this line
+    algorithm.q = torch.zeros(3)
 
-    print(eval_loader_names)
-    print(eval_loaders)
+    train_minibatches_iterator = zip(*train_loaders)
+    checkpoint_vals = collections.defaultdict(lambda: [])
+
+    def save_checkpoint(filename):
+        if args.skip_model_save:
+            return
+        save_dict = {
+            "args": vars(args),
+            "model_input_shape": dataset.input_shape,
+            "model_num_classes": dataset.num_classes,
+            "model_num_domains": len(dataset) - len(args.test_envs),
+            "model_hparams": hparams,
+            "model_dict": algorithm.state_dict()
+        }
+        torch.save(save_dict, os.path.join(args.output_dir, filename))
 
     evals = zip(eval_loader_names, eval_loaders, eval_weights)
-    for name, loader, weights in evals:
-        descs = misc.descriptor(feat_extr, loader, weights, device)
-        print(descs.shape)
-        print(descs[0])
-        # Change to erm/descs.... for ERM
-        torch.save(descs, "./domainbed/erm/descs_"+name+".pt")
 
-  
+    # Loading from path
+    PATH = args.load_model_dir + "model.pkl"
+    # PATH = "../ermnew/model.pkl"
+    trained_model = algorithm
+    trained_model.load_state_dict(torch.load(PATH)['model_dict'])
+
+    feat_extr = trained_model.featurizer.to(device)    
+
+    print(feat_extr)
+
+    # Start Evaluation
+    results = {}
+    train_data = {}
+    test_data = {} 
+
+    train_x = []
+    train_y = []
+    train_z = []
+    train_g = []
+    train_preds = []
+    train_logits = []
+
+    test_x = []
+    test_y = []
+    test_z = []
+    test_g = []
+    test_preds = []
+    test_logits = []
+    torch.cuda.empty_cache()
+
+
+    print("Starting to parse....")
+
+    for name, loader, weights in evals:
+        print("Current Split: ", name)
+        acc = misc.accuracy(algorithm, loader, weights, device)
+
+        x, y, z, p, l = misc.return_x_y_z(feat_extr, loader, weights, device, algorithm)
+        g = torch.full(y.shape, int(name[3]))
+
+        if 'in' in name:
+            train_x.append(x)
+            train_y.append(y)
+            train_z.append(z)
+            train_g.append(g)
+            train_preds.append(p)
+            train_logits.append(l)
+        else:
+            test_x.append(x)
+            test_y.append(y)
+            test_z.append(z)
+            test_g.append(g)
+            test_preds.append(p)
+            test_logits.append(l)
+        
+    train_data['feature'] = torch.cat(train_z).detach().cpu().numpy()
+    train_data['label'] = torch.cat(train_y).detach().cpu().numpy()
+    train_data['pred'] = torch.cat(train_preds).detach().cpu().numpy()
+    train_data['group'] = torch.cat(train_g).detach().cpu().numpy()
+    train_data['logits'] = torch.cat(train_logits).detach().cpu().numpy()
+
+    test_data['feature'] = torch.cat(test_z).detach().cpu().numpy()
+    test_data['label'] = torch.cat(test_y).detach().cpu().numpy()
+    test_data['pred'] = torch.cat(test_preds).detach().cpu().numpy()
+    test_data['group'] = torch.cat(test_g).detach().cpu().numpy()
+    test_data['logits'] = torch.cat(test_logits).detach().cpu().numpy()
+
+
+    for key,val in test_data.items():
+        print(key, val.shape)
+
+    for key,val in train_data.items():
+        print(key, val.shape)
+        if key == 'group':
+            print(val)
+
+    save_dir = args.save_parsed_data_dir
+
+    # Save Train Split
+    fname =  f'groupdro_pacs_train_data.p'
+    pickle.dump(train_data, open(save_dir + '/' + fname, 'wb'))
+
+    # Save Test Split
+    fname =  f'groupdro_pacs_test_data.p'
+    pickle.dump(test_data, open(save_dir + '/' + fname, 'wb'))
+
+       
+    print("Finished! Parsed data has been saved to :", save_dir + '/' + fname)
